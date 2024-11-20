c52fe7477b6e9a9739a8e44dee241e4c3ee8a3e9
Frank <ashishfrank1234@gmail.com>
2024-11-20 11:02:21 +0530

    components name updated

--- src/components/header/Searchbar.jsx
+++ src/components/header/Searchbar.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext, useEffect } from 'react'
+import { useContext, useEffect } from 'react'
 import { ProductsContext } from '../../state/context/ProductsContext'
 
 const SearchBar = () => {
